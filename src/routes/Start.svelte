50d394643075cc937374e00482d28841d827fd8c
sonjavanteese <35209042+sonjavanteese@users.noreply.github.com>
2023-02-20 13:54:33 +0100

    ua

---
+++ src/routes/Start.svelte
@@ -0,0 +1,37 @@
+<script>
+  import svelteLogo from "../assets/svelte.svg";
+</script>
+
+<section class="page">
+    <div class="container mx-auto px-4">
+      <header class="flex">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" class="logo" alt="Vite Logo" />
+        </a>
+        <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+          <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+        </a>
+      </header>
+      <article class="nwp-prose">
+        <h1>Header</h1>
+        <h2>Vite + Svelte</h2>
+        <h4>Nwp-Studio</h4>
+        <p>
+          Check out <a
+            href="https://github.com/sveltejs/kit#readme"
+            target="_blank"
+            rel="noreferrer">SvelteKit</a
+          >
+          , the official Svelte app framework powered by Vite!
+        </p>
+
+        <p class="read-the-docs">
+          Click on the Vite and Svelte logos to learn more
+        </p>
+      </article>
+    </div>
+  </section>
+
+<style>
+  /* your styles go here */
+</style>
